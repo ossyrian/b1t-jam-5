d1dc2b82b53c56152d80d3349929cd2c970290f6
rakku <rakku@neden.gg>
2026-08-05 21:50:41 -0700

    furniture sprites

--- scripts/player.gd
+++ scripts/player.gd
@@ -27,13 +27,13 @@ func _move():
 		return
 
 	var input_direction = Input.get_vector("left", "right", "up", "down")
-
-	if input_direction == Vector2.ZERO:
-		sprite.play("idle")
-	else:
-		sprite.play("walk")
-	if input_direction.x != 0:
-		sprite.flip_h = input_direction.x < 0
+	# XXX: disabling while animated sprite isn't on remote
+	#if input_direction == Vector2.ZERO:
+	#sprite.play("idle")
+	#else:
+	#sprite.play("walk")
+	#if input_direction.x != 0:
+	#sprite.flip_h = input_direction.x < 0
 
 	velocity = input_direction * speed
 
